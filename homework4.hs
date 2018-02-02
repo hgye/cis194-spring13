bfaeff3237e940c8d89365a7a1e64ed18deac4fd
hegel.ye <hegel.ye@calix.com>
2018-02-02 16:08:17 +0800

    hw4, ex1

--- homework4.hs
+++ homework4.hs
@@ -30,4 +30,91 @@ fun1' (x:xs)
   | even x = (x - 2) * fun1' xs
   | otherwise = fun1' xs
 
--- fun2 :: Integer -> Integer
+fun2' :: Integer -> Integer
+fun2' = foldr (+) 0.filter even.takeWhile (>1).iterate g
+  where g x | even x  = div x 2
+            | otherwise = 3*x+1
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+
+{-
+Exercise 2: Folding with trees
+Recall the definition of a binary tree data structure. The height of
+Binary_tree a binary tree is the length of a path from the root to the deepest
+node. For example, the height of a tree with a single node is 0; the
+height of a tree with three nodes, whose root has two children, is 1;
+and so on. A binary tree is balanced if the height of its left and right
+subtrees differ by no more than 1, and its left and right subtrees are
+also balanced.
+You should use the following data structure to represent binary
+trees. Note that each node stores an extra Integer representing the
+height at that node.
+data Tree a = Leaf
+| Node Integer (Tree a) a (Tree a)
+deriving (Show, Eq)
+For this exercise, write a function
+foldTree :: [a] -> Tree a
+
+foldTree = ...
+which generates a balanced binary tree from a list of values using
+foldr.
+For example, one sample output might be the following, also visualized
+at right:
+foldTree "ABCDEFGHIJ" ==
+Node 3
+(Node 2
+(Node 0 Leaf ’F’ Leaf)
+’I’
+(Node 1 (Node 0 Leaf ’B’ Leaf) ’C’ Leaf))
+’J’
+(Node 2
+(Node 1 (Node 0 Leaf ’A’ Leaf) ’G’ Leaf)
+’H’
+(Node 1 (Node 0 Leaf ’D’ Leaf) ’E’ Leaf))
+Your solution might not place the nodes in the same exact order,
+but it should result in balanced trees, with each subtree having a
+correct computed height.
+-}
+data Tree a = Leaf
+  | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+--foldTree :: [a] -> Tree a
+--foldTree 
+
+{-
+Exercise 3: More folds!
+1. Implement a function
+xor :: [Bool] -> Bool
+which returns True if and only if there are an odd number of True
+values contained in the input list. It does not matter how many
+False values the input list contains. For example,
+xor [False, True, False] == True
+xor [False, True, False, False, True] == False
+Your solution must be implemented using a fold.
+2. Implement map as a fold. That is, complete the definition
+map’ :: (a -> b) -> [a] -> [b]
+map’ f = foldr ...
+in such a way that map’ behaves identically to the standard map
+function.
+3. (Optional) Implement foldl using foldr. That is, complete the
+definition
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f base xs = foldr ...
+in such a way that myFoldl behaves identically to the standard
+foldl function.
+Hint: Study how the application of foldr and foldl work out:
+foldr f z [x1, x2, ..., xn] == x1 ‘f‘ (x2 ‘f‘ ... (xn ‘f‘ z)...)
+foldl f z [x1, x2, ..., xn] == (...((z ‘f‘ x1) ‘f‘ x2) ‘f‘...) ‘f‘ xn
+-}
+xor :: [Bool] -> Bool
+xor = foldr g False
+  where g a b | a == b = False
+              | otherwise = True
+
+--map' :: (a -> b) -> [a] -> [b]
+--map' f = foldr 
