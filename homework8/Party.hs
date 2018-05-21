2d59a05cc384c40532d91a3f245779ea0f18ad62
hegel.ye <hegel.ye@calix.com>
2018-05-21 16:54:04 +0800

    still not successfully try on maxFun, since GL could only return a one Employee

--- homework8/Party.hs
+++ homework8/Party.hs
@@ -36,27 +36,33 @@ treeFlatten = treeFold [] (\x list -> foldr (++) [x] list)
 --                           _  -> foldr (+) 0 list)
 
 combineGLs :: Employee -> [GuestList] -> GuestList
+-- combineGLs _ [] = GL [] 0
+-- combineGLs _ [e1] = glCons 
 --combineGLs e [] = glCons e (GL [] 0)
-combineGLs _ [] = GL [] 0
-combineGLs e (x:xs) =  moreFun (glCons e (GL [] 0))
-  $ moreFun x (combineGLs e xs)
+--combineGLs _ [] = GL [] 0
+combineGLs e = foldr moreFun (GL [e] 0)
+--combineGLs e (x:xs) =  moreFun (glCons e (GL [] 0))
+--  $ moreFun x (combineGLs e xs)
    --True -> x
    --_ -> combineGLs e xs
 
 
 -- ex3
-nextLevel :: Employee -> [(GuestList, GuestList)]
-  -> (GuestList, GuestList)
-nextLevel e@(Emp _ f) [] = (GL [e] f, GL [] 0)
-nextLevel e ((x,y) : xs)
-  | y > snd (nextLevel  e xs)= (x,y)
-  | otherwise = nextLevel e xs
-
+nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
+nextLevel e twoGl =  (
+  (combineGLs e (map fst twoGl)),
+  (foldr moreFun (GL [] 0) (map snd twoGl))
+  )
 -- ex4
 maxFun :: Tree Employee -> (GuestList, GuestList)
+maxFun = treeFold (GL [] 0, GL [] 0) nextLevel
 --maxFun t@(n@(Node e@(Emp _ f) ) _) treeFold (GL [e] f, GL [] 0) nextLevel t
-maxFun t@(Node (Emp e f) _) = treeFold (GL [Emp e f] f, GL [] 0) nextLevel t
+--maxFun t@(Node (Emp e f) _) = treeFold (GL [Emp e f] f, GL [] 0) nextLevel t
 
+-- maxFun' using combineGLs as fold function, which only could return max fun Employee as GL [E] f
+-- why? because at last, a node of tree is N (E) [],
+-- leaf is empty, could only return GL [E] f or GL [] 0
+-- GL could never be such as GL (E1+E2) f1+f2
 maxFun' :: Tree Employee -> GuestList
 maxFun' = treeFold (GL [] 0) combineGLs
 
