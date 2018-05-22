3c753b6a089fc1d6dde4b2434c6065d2f796afd2
hegel.ye <hegel.ye@calix.com>
2018-05-22 18:20:20 +0800

    finished homework8

--- homework8/Party.hs
+++ homework8/Party.hs
@@ -18,17 +18,20 @@ moreFun gl1@(GL el1 _) gl2@(GL el2 _)
         f2 = foldr (\ e lst -> glCons e lst) (GL [] 0) el2
 
 -- ex2
-treeFold ::  b -> ( a -> [b] -> b) -> Tree a -> b
-treeFold e f (Node root forest) =  f root $ map (treeFold e f) forest
+-- catamophosim, homework3
+treeFold ::  ( a -> [b] -> b) -> Tree a -> b
+--treeFold  f (Node root forest) =  f root $ map (treeFold f) forest
+treeFold f = go where
+  go (Node root forest) = f root (map go forest)
 
 treeSize :: Tree a -> Integer
-treeSize = treeFold 1 (\ _ list -> foldr (+) 1 list )
+treeSize = treeFold (\ _ list -> foldr (+) 1 list )
 
 treeSum :: Tree Integer -> Integer
-treeSum  = treeFold 0 (\ x list -> foldr (+) x list)
+treeSum  = treeFold (\ x list -> foldr (+) x list)
 
 treeFlatten :: Tree a -> [a]
-treeFlatten = treeFold [] (\x list -> foldr (++) [x] list)
+treeFlatten = treeFold (\x list -> foldr (++) [x] list)
 
 -- treeDepth :: Tree a -> Integer
 -- treeDepth = treeFold 1 (\ _ list -> case list of
@@ -36,45 +39,65 @@ treeFlatten = treeFold [] (\x list -> foldr (++) [x] list)
 --                           _  -> foldr (+) 0 list)
 
 combineGLs :: Employee -> [GuestList] -> GuestList
--- combineGLs _ [] = GL [] 0
--- combineGLs _ [e1] = glCons 
---combineGLs e [] = glCons e (GL [] 0)
---combineGLs _ [] = GL [] 0
-combineGLs e = foldr moreFun (GL [e] 0)
---combineGLs e (x:xs) =  moreFun (glCons e (GL [] 0))
---  $ moreFun x (combineGLs e xs)
-   --True -> x
-   --_ -> combineGLs e xs
-
+combineGLs e = foldr moreFun (GL [e] (empFun e)) 
 
 -- ex3
 nextLevel :: Employee -> [(GuestList, GuestList)] -> (GuestList, GuestList)
-nextLevel e twoGl =  (
-  (combineGLs e (map fst twoGl)),
-  (foldr moreFun (GL [] 0) (map snd twoGl))
-  )
--- ex4
-maxFun :: Tree Employee -> (GuestList, GuestList)
-maxFun = treeFold (GL [] 0, GL [] 0) nextLevel
---maxFun t@(n@(Node e@(Emp _ f) ) _) treeFold (GL [e] f, GL [] 0) nextLevel t
---maxFun t@(Node (Emp e f) _) = treeFold (GL [Emp e f] f, GL [] 0) nextLevel t
+nextLevel e lst = (glwithboss, glwithoutboss) where
+  -- glwithoutboss = foldr (<>) (GL [] 0) $ map snd lst
+  glwithoutboss = moreFun
+    (foldr moreFun (GL [] 0) $ map snd lst)
+    (foldr (<>) (GL [] 0) $ map snd lst)
+  glwithboss = moreFun
+    (foldr moreFun (GL [e] (empFun e)) $ map fst lst)
+    (foldr (<>) (GL []  0) $ map fst lst)
+  -- glwithoutboss = combineGLs e $ map snd lst
 
+-- ex4
+maxFun :: Tree Employee -> GuestList
+maxFun t
+  | gl1 > gl2 = gl1
+  | otherwise = gl2
+  where
+    (gl1, gl2) = treeFold nextLevel t
 -- maxFun' using combineGLs as fold function, which only could return max fun Employee as GL [E] f
 -- why? because at last, a node of tree is N (E) [],
 -- leaf is empty, could only return GL [E] f or GL [] 0
 -- GL could never be such as GL (E1+E2) f1+f2
 maxFun' :: Tree Employee -> GuestList
-maxFun' = treeFold (GL [] 0) combineGLs
+maxFun' = treeFold combineGLs
+
+-- this is the version could directlly get the maxFun of (tree e)
+combineGLs' :: Employee -> [GuestList] -> GuestList
+combineGLs' e gl = moreFun (foldr moreFun (GL [e] (empFun e)) gl)
+ (foldr (<>) (GL [] 0) gl)
+
+maxFun'' :: Tree Employee -> GuestList
+maxFun'' = treeFold combineGLs'
 
 tree1 :: Tree Employee
 tree1 = Node (Emp "Stan" 2)
-  [Node (Emp "Bob" 1)
+  [Node (Emp "Bob" 18) 
    [ Node (Emp "Joe" 5) [],
      Node (Emp "John" 3) []
    ],
-   Node (Emp "x" 12)
+   Node (Emp "x" 12) 
    [
      Node (Emp "x" 7) [],
      Node (Emp "y" 17) []
    ]
   ]
+
+-- ex5
+putEmployeeLn :: Employee -> IO ()
+putEmployeeLn e = putStrLn (empName e)
+
+putGuestList :: GuestList -> IO ()
+putGuestList (GL l f) = do
+  putStrLn ("Total fun:" ++ show f)
+  mapM_ putEmployeeLn l
+
+main :: IO ()
+main = do
+  readFile "company.txt" >>= putGuestList.maxFun.read
+
